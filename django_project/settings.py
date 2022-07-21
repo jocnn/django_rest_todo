bb4b5fbd4a684060117fe87da9dcbb9a93f0907a
jocnn <ijann_@outlook.com>
2022-07-21 09:03:11 -0300

    se agregó rest_framework al proyecto

--- django_project/settings.py
+++ django_project/settings.py
@@ -39,7 +39,15 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # Local
     "todos.apps.TodosConfig", 
+    # 3rd party
+    "rest_framework", 
 ]
+
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.AllowAny",
+    ],
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
