2d6d5809dfe35914308a4c8706e40177553d66b0
jocnn <ijann_@outlook.com>
2022-07-22 13:03:29 -0300

    CSRF_TRUSTED_ORIGINS cambio a http / https

--- django_project/settings.py
+++ django_project/settings.py
@@ -72,7 +72,7 @@ CORS_ALLOWED_ORIGINS = (
     "http://localhost:8000",
 )
 
-CSRF_TRUSTED_ORIGINS = ["localhost:3000"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
 
 ROOT_URLCONF = "django_project.urls"
 
